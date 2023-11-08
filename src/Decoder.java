2fee795fae3153c847d5edfc6b1f3e9a7cb8649d
BT_4N2_07 <TracyZ14@nycstudents.net>
2023-11-08 14:07:36 -0500

    Finished the encoder and decoder for the atbash cipher and the encoder for the caesar cipher

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,4 +1,30 @@
 public class Decoder
 {
+    public String originalMessage;
 
+    public Decoder(String originalMessage)
+    {
+        this.originalMessage = originalMessage;
+    }
+
+    public String atbashDecoder()
+    {
+        String decryptedMessage = "";
+        String[] originalLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] decryptedLetters = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
+        for(int index = 0; index < originalMessage.length(); index++)
+        {
+            String letter = originalMessage.substring(index, index + 1);
+            int arrayIndex = 0;
+            for(int i = 0; i < 25; i++)
+            {
+                if(originalLetters[i].equals(letter))
+                {
+                    arrayIndex = i;
+                }
+            }
+            decryptedMessage = decryptedMessage + decryptedLetters[arrayIndex];
+        }
+        return decryptedMessage;
+    }
 }
--- src/Encoder.java
+++ src/Encoder.java
@@ -3,22 +3,50 @@ public class Encoder
     private String originalMessage;
     private String[] lowerCaseLetters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
     private String[] upperCaseLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    // Substitutes each letter of the alphabet with another letter by shifting the whole alphabet forward by a certain number of letters
-    public String caesarCipher(int shift)
+
+    public Encoder(String originalMessage)
+    {
+        this.originalMessage = originalMessage;
+    }
+
+    public String atbashEncoder()
     {
         String encryptedMessage = "";
-        String originalCharacter = "";
-        for(int i = 0; i == originalMessage.length(); i++)
+        String[] originalLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] encryptedLetters = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
+        for(int index = 0; index < originalMessage.length(); index++)
         {
-            if(i == originalMessage.length())
+            String letter = originalMessage.substring(index, index + 1);
+            int arrayIndex = 0;
+            for(int i = 0; i < 25; i++)
             {
-                originalCharacter = originalMessage.substring(i);
+                if(originalLetters[i].equals(letter))
+                {
+                    arrayIndex = i;
+                }
             }
-            else
-            {
-                originalCharacter = originalMessage.substring(i, i + 1);
-            }
+            encryptedMessage = encryptedMessage + encryptedLetters[arrayIndex];
+        }
+        return encryptedMessage;
+    }
 
+    public String caesarCipherEncoder(int shift)
+    {
+        String encryptedMessage = "";
+        String[] originalLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        for(int index = 0; index < originalMessage.length(); index++)
+        {
+            String letter = originalMessage.substring(index, index + 1);
+            int arrayIndex = 0;
+            for(int i = 0; i < 25; i++)
+            {
+                if(originalLetters[i].equals(letter))
+                {
+                    arrayIndex = i;
+                }
+            }
+            int shiftedLetterIndex = (arrayIndex + shift) % 26;
+            encryptedMessage = encryptedMessage + originalLetters[shiftedLetterIndex];
         }
         return encryptedMessage;
     }
