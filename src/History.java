3eb614ee23b55aed0e8c99aca14939d6638cb1be
BT_4N2_07 <TracyZ14@nycstudents.net>
2023-11-10 14:08:06 -0500

    Finished the History class and started the encryption in the Main class

--- src/History.java
+++ src/History.java
@@ -1,7 +1,15 @@
 public class History
 {
     private String originalMessage;
+    private String encryptionDecryption;
+    private String cipherUsed;
     private String encryptedDecryptedMessage;
-    private int cipherUsed;
 
+    public History(String originalMessage, String encryptionDecryption, String cipherUsed, String encryptedDecryptedMessage)
+    {
+        this.originalMessage = originalMessage;
+        this.encryptionDecryption = encryptionDecryption;
+        this.cipherUsed = cipherUsed;
+        this.encryptedDecryptedMessage = encryptedDecryptedMessage;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int nextAction = 0;
-        for (int i = 0; nextAction != 4; i = i + 0)
+        String changedMessage = "";
+        int cipherNumber = 0;
+        String cipher = "";
+        String[] history = {};
+        for (int i = 0; nextAction != 4; i = i)
         {
             System.out.println("Enter:");
             System.out.println("  '1' to encrypt message");
@@ -13,15 +18,37 @@ public class Main {
             nextAction = Integer.parseInt(s.nextLine());
             if(nextAction == 1)
             {
-
+                System.out.println("Enter:");
+                System.out.println("  '1' for atbash");
+                System.out.println("  '2' for caesar cipher");
+                cipherNumber = Integer.parseInt(s.nextLine());
+                System.out.print("Enter the message you want to encrypt: ");
+                Encoder message = new Encoder(s.nextLine());
+                if(cipherNumber == 1)
+                {
+                    changedMessage = message.atbashEncoder();
+                    cipher = "Atbash";
+                }
+                if(cipherNumber == 2)
+                {
+                    System.out.print("Enter the number of letters you want to shift by: ");
+                    int shift = Integer.parseInt(s.nextLine());
+                    changedMessage = message.caesarCipherEncoder(shift);
+                    cipher = "Caesar Cipher";
+                }
+                History entry = new History(changedMessage, "Encryption", cipher, changedMessage);
+                
             }
             if(nextAction == 2)
             {
-
+                System.out.println("Enter:");
+                System.out.println("  '1' for atbash");
+                System.out.println("  '2' for caesar cipher");
+                cipherNumber = Integer.parseInt(s.nextLine());
             }
             if(nextAction == 3)
             {
-
+                System.out.println(Arrays.toString(history));
             }
         }
         System.out.println("This program has stopped running.");
